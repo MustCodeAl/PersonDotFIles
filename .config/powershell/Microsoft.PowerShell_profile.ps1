6107a47683c982e0eaf69b77b35feec46217d0fa
Albert <87888006+MustCodeAl@users.noreply.github.com>
2023-11-25 01:55:41 -0600

    Add files via upload

---
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+oh-my-posh init pwsh | Invoke-Expression
+
+
+if(Test-Path '/Users/albertdoh/.inshellisense/key-bindings-pwsh.ps1' -PathType Leaf){. /Users/albertdoh/.inshellisense/key-bindings-pwsh.ps1}
