1b2bc104fb9a911a9137d9665379c394015feb63
Albert <87888006+MustCodeAl@users.noreply.github.com>
2024-02-03 01:24:05 -0600

    Update Microsoft.PowerShell_profile.ps1

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,1 @@
 oh-my-posh init pwsh | Invoke-Expression
-
-
-if(Test-Path '/Users/albertdoh/.inshellisense/key-bindings-pwsh.ps1' -PathType Leaf){. /Users/albertdoh/.inshellisense/key-bindings-pwsh.ps1}
